da9ce35e6fcc20de3182c5cc9ba05238d82ed3d5
Fajar DA <126342447+Fajarian94@users.noreply.github.com>
2023-06-15 13:51:36 +0700

    Update single _inked_list_game.py

--- Pertemuan 14/single _inked_list_game.py
+++ Pertemuan 14/single _inked_list_game.py
@@ -3,14 +3,11 @@ class Item:
         self.nama = nama
         self.kepentingan = kepentingan
         self.next = None
-
 class Tas:
     def __init__(self):
         self.head = None
-
     def tambahkan_item(self, nama, kepentingan):
         item_baru = Item(nama, kepentingan)
-
         if self.head is None:
             self.head = item_baru
         else:
@@ -24,29 +21,22 @@ class Tas:
                     current_item = current_item.next
                 item_baru.next = current_item.next
                 current_item.next = item_baru
-
     def hapus_item(self, nama):
         current_item = self.head
-
         if current_item is not None:
             if current_item.nama == nama:
                 self.head = current_item.next
                 return
-
         while current_item is not None:
             if current_item.nama == nama:
                 break
             prev_item = current_item
             current_item = current_item.next
-
         if current_item is None:
             return
-
         prev_item.next = current_item.next
-
     def cetak_daftar_item(self):
         current_item = self.head
-
         if current_item is None:
             print("Tas kosong")
         else:
@@ -57,14 +47,12 @@ class Tas:
 
 # Contoh penggunaan
 tas = Tas()
-
 while True:
     print("\nMenu:")
     print("1. Tambahkan item ke dalam tas")
     print("2. Hapus item dari tas")
     print("3. Cetak daftar item dalam tas")
     print("4. Keluar")
-
     pilihan = input("Pilih menu (1/2/3/4): ")
 
     if pilihan == "1":
@@ -72,18 +60,14 @@ while True:
         kepentingan = int(input("Masukkan tingkat kepentingan item: "))
         tas.tambahkan_item(nama, kepentingan)
         print("Item ditambahkan ke dalam tas!")
-
     elif pilihan == "2":
         nama = input("Masukkan nama item yang akan dihapus: ")
         tas.hapus_item(nama)
         print("Item dihapus dari tas!")
-
     elif pilihan == "3":
         tas.cetak_daftar_item()
-
     elif pilihan == "4":
         print("Terima kasih!")
         break
-
     else:
         print("Pilihan tidak valid. Silakan pilih menu yang tersedia.")
