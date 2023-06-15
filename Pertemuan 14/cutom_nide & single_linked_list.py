14014ebb8464a8363b898d0f84e3b5f1b421638e
Fajar DA <126342447+Fajarian94@users.noreply.github.com>
2023-06-15 13:35:18 +0700

    Update cutom_nide & single_linked_list.py

--- Pertemuan 14/cutom_nide & single_linked_list.py
+++ Pertemuan 14/cutom_nide & single_linked_list.py
@@ -4,17 +4,14 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-
 class Linkedlist :
     def __init__(self):
         self.head = None
-
 # Menambahkan Elemen Linked List
 def add_to_front(self,data) :
     new_node = Node(data)
     new_node.next = self.head
     self.head = new_node
-
 def add_to_end (self,data):
     new_node = Node(data)
     if self.head is None:
@@ -24,36 +21,28 @@ def add_to_end (self,data):
     while current.next :
         current = current.next
     current.next = new_node
-
 # Menghapus element list
-
 def remove_front(self):
     if self.head is None :
         return
     self.head = self.head.next
-
 def remove_end(self):
     if self.head is None :
         return
-    
     if self.head.next is None :
         self.head = None
         return
-    
     current =   self.head
     while current.next.next :
         current = current.next
     current.next = None
-
 #  Mencetak Isi Linked list
-
 def print_linked_list(self):
     current = self.head
     while current :
         print(current.data, end = " ")
         current = current.next
     print()
-
 #  membuat dan mencetak linked list
 
 linked_list = Linkedlist()
